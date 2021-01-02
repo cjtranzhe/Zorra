8a9c0a7387c93b1d1a74742818c8b4983287a8bc
intelhistorydotnetatgmaildotcom <59816388+intelhistorydotnetatgmaildotcom@users.noreply.github.com>
2021-01-02 08:41:20 -0500

    Front – Multipurpose Responsive Template

--- UI.Web/Data/CRUD/Read.cs
+++ UI.Web/Data/CRUD/Read.cs
@@ -1,10 +1,23 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UI.Web.Data.Entities.PROD;
 
 namespace UI.Web.Data.CRUD
 {
     public class Read
     {
+        //public static Tuple<List<dynamic>, List<dynamic>> ReadTuple(string email)
+        //{
+        //    using var context1 = new zorrarth_archiveContext();
+        //    var c = context1.P4hCreators.Where(z => z.Email == email).ToList();
+
+        //    using var context2 = new zorrarth_archiveContext();
+        //    var cd = context2.P4hProjects.Where(z => z.IdGuid == c[0].IdGuid).ToList();
+
+        //    return Tuple.Create(c, cd);
+        //}
+
         public static dynamic Single(int id)
         {
             using var context = new zorrarth_archiveContext();
--- UI.Web/Data/CRUD/Update.cs
+++ UI.Web/Data/CRUD/Update.cs
@@ -1,10 +1,27 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using UI.Web.Data.Entities.PROD;
 
 namespace UI.Web.Data.CRUD
 {
     public class Update
     {
+        //public static async Task asyncUpdate(int id)
+        //{
+        //    using var context = new zorrarth_archiveContext();
+        //    var query = from t in context.SiteVisits
+        //                where t.Id == id
+        //                select t;
+
+        //    foreach (var item in query)
+        //    {
+        //        item.DateCreated = DateTime.Now;
+        //    }
+
+        //    await context.SaveChangesAsync();
+        //}
+
         public static void Entity(int id)
         {
             using var context = new zorrarth_archiveContext();
