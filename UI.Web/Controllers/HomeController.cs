384f2ca10072ed13f3c1305853f301aa877547c2
intelhistorydotnetatgmaildotcom <59816388+intelhistorydotnetatgmaildotcom@users.noreply.github.com>
2021-01-20 22:27:01 -0500

    ...

--- UI.Web/Controllers/HomeController.cs
+++ UI.Web/Controllers/HomeController.cs
@@ -4,9 +4,21 @@ namespace UI.Web.Controllers
 {
     public class HomeController : Controller
     {
+        public const string DotCsHtml = ".cshtml";
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult LoadMain(string component)
+        {
+            return PartialView($"~/Views/Home/{component}/PartialMain{DotCsHtml}");
+        }
+
+        public PartialViewResult LoadPartial(string component, string partial)
+        {
+            return PartialView($"~/Views/{component}/{partial}{DotCsHtml}");
         }
     }
 }
